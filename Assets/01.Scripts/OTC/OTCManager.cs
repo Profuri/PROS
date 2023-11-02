21b3ee00d8d64a0445141dc85c335d7af347a7a3
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-11-02 18:26:12 +0900

    FEAT 장외 반대 방향 튕기기 구현

--- Assets/01.Scripts/OTC/OTCManager.cs
+++ Assets/01.Scripts/OTC/OTCManager.cs
@@ -19,6 +19,8 @@ public class OTCManager : MonoBehaviour
     }
 
     [SerializeField] private float _otcPower = 10f;
+    [Range(0f, 1f)]
+    [SerializeField] private float _bouncePer;
 
     [Header("Test Parameters")]
     public GameObject _otcObj;
@@ -44,22 +46,60 @@ public class OTCManager : MonoBehaviour
     public void PlayOTC(GameObject otcObj, Vector3 otcPrevPos, Vector3 otcCurPos, Vector3 attackDir)
     {
         Vector3 otcMovingDir = CalcMovingDir(otcPrevPos, otcCurPos);
+        Vector3 otcDir = CalcOTCDir(attackDir.normalized, otcMovingDir);
+        bool isBounce = false;
 
-        Vector3 otcDir = CalcOTCDir(attackDir,  otcMovingDir);
+        // Cases with OTC Direction X or Y of Zero
+
+        Debug.Log(otcDir);
+
+        if (otcDir == Vector3.zero)
+        {
+            otcDir = attackDir;
+            isBounce = true;
+        }
+
+        Debug.Log(otcDir);
+
+        if (otcDir != Vector3.zero)
+        {
+            if (otcDir.x == 0)
+            {
+                int random = Random.Range(0, 2);
+                switch (random)
+                {
+                    case 0:
+                        otcDir.x++;
+                        break;
+                    case 1:
+                        otcDir.x--;
+                        break;
+                }
+            }
+            if (otcDir.y == 0)
+                otcDir.y++;
+        }
+        
+
+        // ToDo : There should be a value to check if it has fallen below the floor. (Need Bool Parameter)**
+        // Protect flying to the floor
         if (otcDir.y < 0)
-            otcDir *= -1;
+            otcDir.y *= -1;
 
         if (otcObj.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
         {
-            rb.AddForce(otcDir * _otcPower, ForceMode2D.Impulse);
+            if (isBounce)
+                rb.AddForce(otcDir * _otcPower, ForceMode2D.Impulse);
+            else
+                rb.AddForce(otcDir * _otcPower * _bouncePer, ForceMode2D.Impulse);
         }
         else
             Debug.LogError("Otc Object Rigidbody2D not Found");
     }
 
-    private Vector3 CalcOTCDir(Vector3 attackDir, Vector3 otcDir)
+    private Vector3 CalcOTCDir(Vector3 attackMoveDir, Vector3 otcMoveDir)
     {
-        return (attackDir + otcDir).normalized;
+        return (attackMoveDir + otcMoveDir).normalized;
     }
 
     private Vector3 CalcMovingDir(Vector3 prevPos, Vector3 curPos)
@@ -70,6 +110,5 @@ public class OTCManager : MonoBehaviour
     private void OnDrawGizmos()
     {
         //Test Draw
-        Debug.DrawLine(_otcObj.transform.position, _otcObj.transform.position + CalcOTCDir(_attackDir, CalcMovingDir(_otcPrevPos, _otcCurPos)) * _otcPower, Color.blue);
     }
 }

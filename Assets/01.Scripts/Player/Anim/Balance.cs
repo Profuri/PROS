e6076a3d1fc28d37a19663d02af27b74216f71b9
Nam junseong <danielnam0801@gmail.com>
2023-11-07 00:10:30 +0900

    FEAT_RAGDOLL Anim

--- Assets/01.Scripts/Player/Anim/Balance.cs
+++ Assets/01.Scripts/Player/Anim/Balance.cs
@@ -7,45 +7,17 @@ public class Balance : MonoBehaviour
     public float TargetRotation;
     public Rigidbody2D Rb;
     public float Force = 500;
-    //public float AddForceValue = -40;
-    //public float RepressionForceValue = 1;
 
     private HingeJoint2D _hingeJoint;
-    //private Balance _ptBalance;
-    //private Rigidbody2D _ptRb;
+
     private void Awake()
     {
         Rb = GetComponent<Rigidbody2D>();
         _hingeJoint = GetComponent<HingeJoint2D>();
     }
 
-    private void Start()
-    {
-        //_ptBalance = _hingeJoint.connectedBody.GetComponent<Balance>();
-        //_ptRb = _ptBalance.GetComponent<Rigidbody2D>();
-        //_hingeJoint.useMotor = true;
-    }
-
     public void Update()
-    {
+    {        
         Rb.MoveRotation(Mathf.LerpAngle(Rb.rotation, TargetRotation, Force * Time.deltaTime));
-        //float targetRot;
-        //TargetRotation = _ptBalance.TargetRotation;
-        //targetRot = TargetRotation;
-
-        //if (transform.CompareTag("PowerExcept"))
-        //{
-        //    targetRot = TargetRotation;
-        //}
-        //else
-        //{
-        //    Debug.Log(_ptBalance.TargetRotation);
-        //    TargetRotation = _ptBalance.TargetRotation;
-        //    targetRot = (_ptBalance.TargetRotation == 0)
-        //      ? TargetRotation : TargetRotation + (AddForceValue / RepressionForceValue);
-        //}
-        // _hingeJoint.motor Mathf.Lerp()
-        //Rb.MoveRotation(Mathf.LerpAngle(Rb.rotation
-        //    , targetRot, Force * Time.fixedDeltaTime));
     }
 }
--- Assets/01.Scripts/Player/Anim/TestPlayerController.cs
+++ Assets/01.Scripts/Player/Anim/TestPlayerController.cs
@@ -10,77 +10,71 @@ public class TestPlayerController : MonoBehaviour
     [SerializeField] Rigidbody2D _rightRb;
     [SerializeField] float _jumpForce;
     [SerializeField] float _playerSpeed;
-    [SerializeField] float _stepwait = 0.3f; 
     [SerializeField] LayerMask _ground;
     [SerializeField] Transform _playerPos;
     [SerializeField] float positionRadius;
 
-    private bool _isOnGround;
+    //[SerializeField] float _stepwait = 0.3f; 
+    private bool _isOnGround = false;   
+    private bool _isJumping = false;
 
     private void Update()
     {
-        _isOnGround = Physics2D.OverlapCircle(_playerPos.position, positionRadius, _ground);
-        if (_isOnGround == true && Input.GetKeyDown(KeyCode.Space))
-        {
-            _animator.Play("Jump");
-            Debug.Log("Jump");
-            // _rb.AddForce(Vector2.up * _jumpForce);
-        }
-        if (_isOnGround == true) return;
-
+        if (_isJumping) return;
         if (Input.GetAxisRaw("Horizontal") != 0)
         {
             if (Input.GetAxisRaw("Horizontal") > 0)
             {
+                float x = Input.GetAxisRaw("Horizontal");
+                transform.position += new Vector3(x, 0) * _playerSpeed * Time.deltaTime;
                 _animator.Play("WalkRight");
-
-                _rightRb.AddForce(Vector2.right * _playerSpeed * Time.deltaTime, ForceMode2D.Impulse);
-                _leftRb.AddForce(Vector2.right * _playerSpeed * Time.deltaTime, ForceMode2D.Impulse);
-                StartCoroutine(MoveRight(_stepwait));
-               
-                //transform.position += new Vector3(moveValue.x, moveValue.y);
-                //rb.AddForce(moveValue);
             }
             else
             {
-                _rightRb.AddForce(Vector2.left * _playerSpeed * Time.deltaTime, ForceMode2D.Impulse);
-                _leftRb.AddForce(Vector2.left * _playerSpeed * Time.deltaTime, ForceMode2D.Impulse);
+                float x = Input.GetAxisRaw("Horizontal");
+                transform.position += new Vector3(x, 0) * _playerSpeed * Time.deltaTime;
                 _animator.Play("WalkLeft");
-                StartCoroutine(MoveLeft(_stepwait));
-                //transform.position += new Vector3(moveValue.x, moveValue.y);
-                //rb.AddForce(moveValue);
             }
         }
         else
         {
-            _leftRb.velocity = new Vector3(0, _leftRb.velocity.y);
-            _rightRb.velocity = new Vector3(0, _rightRb.velocity.y);
-            //_leftRb.velocity = Vector3.zero;
-            //_rightRb.velocity = Vector3.zero;
             _animator.Play("Idle");
+        }
+
+        _isOnGround = Physics2D.OverlapCircle(_playerPos.position, positionRadius, _ground);
+        if (_isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            _isJumping = true;
+            _animator.Play("Jump");
+            _isOnGround = false;
         }
 
 
     }
-
     public void Jump()
     {
+        _isJumping = false;
         _rb.AddForce(Vector2.up * _jumpForce);
+        Debug.Log("Jump");
     }
 
-    IEnumerator MoveRight(float waitTime)
-    {
-        _rightRb.AddRelativeForce(Vector2.right * _playerSpeed * Time.deltaTime);
-        //_rightRb.AddForce(Vector2.right * _playerSpeed * Time.deltaTime, ForceMode2D.Impulse);
-        yield return new WaitForSeconds(waitTime);
-        _leftRb.AddRelativeForce(Vector2.right * _playerSpeed * Time.deltaTime);
-    }
+    //IEnumerator MoveRight(float waitTime)
+    //{
+    //    //_rightRb.velocity = (Vector2.right * _playerSpeed * Time.deltaTime);
+    //    _rightRb.AddRelativeForce(Vector2.right * _playerSpeed * Time.deltaTime, ForceMode2D.Force);
+    //    //_rightRb.AddForce(Vector2.right * _playerSpeed * Time.deltaTime, ForceMode2D.Impulse);
+    //    yield return new WaitForSeconds(waitTime);
+    //    //_leftRb.velocity = (Vector2.right * _playerSpeed * Time.deltaTime);
+    //    _leftRb.AddRelativeForce(Vector2.right * _playerSpeed * Time.deltaTime, ForceMode2D.Force);
+    //}
 
-    IEnumerator MoveLeft(float waitTime)
-    {
-        _leftRb.AddRelativeForce(Vector2.left * _playerSpeed * Time.deltaTime);
-        yield return new WaitForSeconds(waitTime);
-        _rightRb.AddRelativeForce(Vector2.left * _playerSpeed * Time.deltaTime);
-    }
+    //IEnumerator MoveLeft(float waitTime)
+    //{
+    //    //_leftRb.velocity = (Vector2.left * _playerSpeed * Time.deltaTime);
+    //    _leftRb.AddRelativeForce(Vector2.left * _playerSpeed * Time.deltaTime, ForceMode2D.Force);
+    //    yield return new WaitForSeconds(waitTime);
+    //    //_rightRb.velocity = (Vector2.left * _playerSpeed * Time.deltaTime);
+    //    _rightRb.AddRelativeForce(Vector2.left * _playerSpeed * Time.deltaTime, ForceMode2D.Force);
+    //}
 
 }

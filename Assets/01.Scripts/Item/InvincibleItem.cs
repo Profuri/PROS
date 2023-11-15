da1a74c9dd46bb834ea450ce18f0b209ec7c5a3b
Nam junseong <danielnam0801@gmail.com>
2023-11-15 09:52:28 +0900

    CHORE_Invincible item move(follow)

--- Assets/01.Scripts/Item/InvincibleItem.cs
+++ Assets/01.Scripts/Item/InvincibleItem.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.Events;
 
 public class InvincibleItem : BaseItem
 {
@@ -10,19 +11,23 @@ public class InvincibleItem : BaseItem
     [SerializeField] private float _detectRadius = 5f;
     [SerializeField] private float _turnAngleValue = 100f;
     [SerializeField] private float _runawayMaxtime = 2f;
-    [SerializeField] private float _minSpeed = 2f;
-    [SerializeField] private float _maxSpeed = 4f;
+    [SerializeField] private float _basePower = 5f;
+    [SerializeField] private float _rangeAreaDivide = 6;
+    [SerializeField] private UnityEvent RunAwayEvt;
     private float _runawayTime;
     private bool _isturn = false;
+    Camera _mainCam;
 
     public override void Init()
     {
+        if (_mainCam == null)
+            _mainCam = Camera.main;
         //_runawayTime = 0;
         //_moveDir = new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f)).normalized;
         //transform.position = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
         //var moveSpeed = Random.Range(1f, 2f);
         //_movementSpeed = moveSpeed;
-        ////HitEvent.AddListener(ShakePosition);
+        //HitEvent.AddListener(ShakePosition);
     }
     public override void OnTakeItem(Player takenPlayer)
     {
@@ -53,73 +58,74 @@ public class InvincibleItem : BaseItem
     public override void UpdateItem()
     {
         base.UpdateItem();
-        RunAwayMove();
+        QuidditchMove();
         transform.Translate(_moveDir * (_movementSpeed * Time.deltaTime), Space.World);
     }
 
-    void RunAwayMove()
+    void QuidditchMove()
     {
         #region 근처 플레이어 찾기.
         Collider2D[] playerCol = Physics2D.OverlapCircleAll(transform.position, _detectRadius, LayerMask.GetMask("DAMAGEABLE"));
 
-        int index = 0; // min Distance collider index;
+        int nearColindex = 0; // min Distance collider index;
         float curDis = 0f;
         float minDis = float.MaxValue;
-
+        //float maxDis = float.MinValue;
         for (int i = 0; i < playerCol.Length; i++)
         {
             curDis = Vector3.Distance(playerCol[i].transform.position, transform.position);
             if (curDis < minDis)
             {
-                index = i;
+                nearColindex = i;
                 minDis = curDis;
             }
         }
 
         #endregion
 
-        Vector3 dir;
+
+        Vector2 centerpos = transform.position;
+        Vector2 targetPos = -(playerCol[nearColindex].transform.position - transform.position).normalized * _basePower;
+        Vector2 viewportPoint = _mainCam.WorldToViewportPoint(targetPos);
+        if (viewportPoint.x <= 0.05 || viewportPoint.x >= 0.9)
+        {
+
+        }
+        if (viewportPoint.y <= 0.05 || viewportPoint.y >= 0.9)// 카메라 범위 벗어난것
+        {
+
+        }
+
         if (playerCol.Length != 0)
         {
             _runawayTime = 0;
-            dir = -(playerCol[index].transform.position - transform.position).normalized;
+            //dir = 
         }
         else
         {
+            _runawayTime = 0;
             if (_runawayTime > _runawayMaxtime)
             {
-                Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
-                _moveDir = (Camera.main.ScreenToWorldPoint(screenCenter) - transform.position).normalized; //중심을 향해 가도록    
+                int x = Screen.width / 2;
+                int y = Screen.height / 2;
+                Vector2 screenCenter =
+                    new Vector2(x + Random.Range(-x / _rangeAreaDivide, x / _rangeAreaDivide),
+                               y + Random.Range(-y / _rangeAreaDivide, y / _rangeAreaDivide));
+
+                _moveDir = (_mainCam.ScreenToWorldPoint(screenCenter) - transform.position).normalized; //중심을 향해 가도록    
             }
-            dir = _moveDir;
+            //dir = _moveDir;
 
             _runawayTime += Time.deltaTime;
         }
 
-        float dot = Vector3.Dot(transform.up, dir);
-        if (dot < 1.0f)
-        {
-            float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
-            Vector3 cross = Vector3.Cross(transform.up, dir);
-            if (cross.z < 0)
-            {
-                angle = transform.rotation.eulerAngles.z - Mathf.Min(_turnAngleValue * Time.deltaTime, angle);
-            }
-            else
-            {
-                angle = transform.rotation.eulerAngles.z + Mathf.Min(_turnAngleValue * Time.deltaTime, angle);
-            }
-            transform.rotation = Quaternion.Euler(0, 0, angle);
-        }
-
-        _moveDir = transform.up;
-        float sinValue = Mathf.Sin(Time.time) / 2 + 0.5f;
-        if (sinValue < 0.3)
-            _movementSpeed = Mathf.Lerp(_minSpeed, _maxSpeed, sinValue);
-        else if (sinValue < 0.8)
-            _movementSpeed = Mathf.Lerp(_minSpeed, _maxSpeed, sinValue *= 3);
-        else
-            _movementSpeed = Mathf.Lerp(_minSpeed, _maxSpeed, sinValue);
+        //float sinValue = Mathf.Sin(Time.time) / 2 + 0.5f;
+        //if (sinValue < 0.3)
+        //    _movementSpeed = Mathf.Lerp(_minSpeed, _maxSpeed, sinValue);
+        //else if (sinValue < 0.8)
+        //    _movementSpeed = Mathf.Lerp(_minSpeed, _maxSpeed, sinValue *= 2);
+        //else
+        //    _movementSpeed = Mathf.Lerp(_minSpeed, _maxSpeed, sinValue);
 
     }
 
--- Assets/01.Scripts/Item/RangeUpItem.cs
+++ Assets/01.Scripts/Item/RangeUpItem.cs
@@ -7,26 +7,25 @@ using DG.Tweening;
 public class RangeUpItem : BaseItem
 {
     [SerializeField] private float _shakeDuration = 0.5f;
-    [SerializeField] private float _detectRadius = 5f;
+    [SerializeField] private float _detectDistance = 5f;
     [SerializeField] private float _turnAngleValue = 100f;
     [SerializeField] private float _runawayMaxtime = 2f;
+    [SerializeField] private float _runawayDelaytime = 2f;
     [SerializeField] private float _minSpeed = 2f;
     [SerializeField] private float _maxSpeed = 4f;
-    [SerializeField] private float _rangeAreaDivide = 6;
     private float _runawayTime;
     private bool _isturn = false;
-    Camera _mainCam;
 
+    Collider2D[] playerCol;
     public override void Init()
     {
-        if(_mainCam == null)
-            _mainCam = Camera.main;
-        //_runawayTime = 0;
-        //_moveDir = new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f)).normalized;
-        //transform.position = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
-        //var moveSpeed = Random.Range(1f, 2f);
-        //_movementSpeed = moveSpeed;
-        ////HitEvent.AddListener(ShakePosition);
+        _runawayTime = 0;
+        _moveDir = new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f)).normalized;
+        transform.position = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+        var moveSpeed = Random.Range(1f, 2f);
+        _movementSpeed = moveSpeed;
+        playerCol = Physics2D.OverlapCircleAll(transform.position, 100f, LayerMask.GetMask("DAMAGEABLE"));
+        //HitEvent.AddListener(ShakePosition);
     }
     public override void OnTakeItem(Player takenPlayer)
     {
@@ -36,73 +35,75 @@ public class RangeUpItem : BaseItem
     public override void GenerateSetting(Vector2 moveDir, Vector2 spawnPos, float movementSpeed)
     {
         base.GenerateSetting(moveDir, spawnPos, movementSpeed);
+        playerCol = Physics2D.OverlapCircleAll(transform.position, 100f, LayerMask.GetMask("DAMAGEABLE"));
         _runawayTime = 0f;
         _isturn = false;
     }
 
-    //private void Start()
-    //{
-    //    Init();
-    //}
+    private void Start()
+    {
+        Init();
+    }
 
 
-    //private void Update()
-    //{
+    private void Update()
+    {
 
-    //    RunAwayMove();
-    //    transform.Translate(_moveDir * (_movementSpeed * Time.deltaTime), Space.World);
+        RunAwayMove();
+        transform.Translate(_moveDir * (_movementSpeed * Time.deltaTime), Space.World);
 
-    //}
+    }
 
     public override void UpdateItem()
     {
         base.UpdateItem();
         RunAwayMove();
-        transform.Translate(_moveDir * (_movementSpeed * Time.deltaTime), Space.World);
     }
 
     void RunAwayMove()
     {
         #region 근처 플레이어 찾기.
-        Collider2D[] playerCol = Physics2D.OverlapCircleAll(transform.position, _detectRadius, LayerMask.GetMask("DAMAGEABLE"));
+        //Collider2D[] playerCol = Physics2D.OverlapCircleAll(transform.position, _detectRadius, LayerMask.GetMask("DAMAGEABLE"));
 
-        int nearColindex = 0; // min Distance collider index;
+        int index = 0; // min Distance collider index;
         float curDis = 0f;
         float minDis = float.MaxValue;
-        //float maxDis = float.MinValue;
+
         for (int i = 0; i < playerCol.Length; i++)
         {
             curDis = Vector3.Distance(playerCol[i].transform.position, transform.position);
             if (curDis < minDis)
             {
-                nearColindex = i;
+                index = i;
                 minDis = curDis;
             }
-            //if(curDis > maxDis)
-            //{
-            //    maxDis = curDis;
-            //}
         }
 
         #endregion
 
         Vector3 dir;
-        if (playerCol.Length != 0)
+        //if (playerCol.Length != 0)
+        if (minDis < _detectDistance)
         {
             _runawayTime = 0;
-            dir = -(playerCol[nearColindex].transform.position - transform.position).normalized;
+            dir = -(playerCol[index].transform.position - transform.position).normalized;
         }
         else
         {
             if (_runawayTime > _runawayMaxtime)
             {
-                int x = Screen.width / 2;
-                int y = Screen.height / 2;
-                Vector2 screenCenter = 
-                    new Vector2(x + Random.Range(-x/ _rangeAreaDivide, x/ _rangeAreaDivide),
-                               y + Random.Range(-y/ _rangeAreaDivide, y / _rangeAreaDivide));
-
-                _moveDir = (_mainCam.ScreenToWorldPoint(screenCenter) - transform.position).normalized; //중심을 향해 가도록    
+                //Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
+                //_moveDir = (Camera.main.ScreenToWorldPoint(screenCenter) - transform.position).normalized; //중심을 향해 가도록    
+                for (int i = 0; i < playerCol.Length; i++)
+                {
+                    curDis = Vector3.Distance(playerCol[i].transform.position, transform.position);
+                    if (curDis < minDis)
+                    {
+                        index = i;
+                        minDis = curDis;
+                    }
+                }
+                _moveDir = (playerCol[index].transform.position - transform.position).normalized;
             }
             dir = _moveDir;
 
@@ -140,6 +141,11 @@ public class RangeUpItem : BaseItem
     {
         Tween shakeTween = transform.DOShakePosition(_shakeDuration);
         shakeTween.Play();
+    }
+
+    private void OnDisable()
+    {
+        playerCol = null;
     }
 
 }

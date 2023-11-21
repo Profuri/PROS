d4dcd95b9f1db277d775c1ac164079676cf8ce1f
ehdbs28 <doyun5373@naver.com>
2023-11-21 21:57:45 +0900

    ADD_맵 장애물 추가

--- Assets/01.Scripts/Core/StageManager.cs
+++ Assets/01.Scripts/Core/StageManager.cs
@@ -100,7 +100,6 @@ public class StageManager : MonoBehaviourPunCallbacks
         //var stageIndex = Random.Range(0, _stageSystems.Count);
         //int stageIndex = 0;
         int stageIndex = (int)NetworkManager.Instance.GetCurRoom.CustomProperties["Mode"];
-        Debug.LogError($"StageIndex: {stageIndex}");
         var mapIndex = Random.Range(0, StageTypeCnt) + 1;
         
         NetworkManager.Instance.PhotonView.RPC(nameof(GenerateNextStageRPC), RpcTarget.All, stageIndex, mapIndex);

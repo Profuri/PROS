927dbdd8d9d2d08999d7bd788c68e26049342794
김준호 <kjhlove0119@naver.com>
2023-11-21 14:57:39 +0900

    CHORE 머지작업

--- Assets/01.Scripts/Player/PlayerDash.cs
+++ Assets/01.Scripts/Player/PlayerDash.cs
@@ -62,7 +62,7 @@ public class PlayerDash : PlayerHandler
 
     private IEnumerator DashCoroutine(float power, Vector3 mouseDir)
     {
-        ParticleManager.Instance.PlayParticleAll("Effect_PlayerDash", _brain.AgentTrm.position);
+        PlayDashEffect(mouseDir);
         AudioManager.Instance.Play(_dashClip);
 
         float timer = 0f;
@@ -297,6 +297,24 @@ public class PlayerDash : PlayerHandler
             //if (_brain.PlayerBuff.CurrentBuff.HasFlag(EBuffType.DOUBLEDASH) && _brain.IsMine)
             //    _isDoubleDash = false;
         }
+    }
+
+    private void PlayDashEffect(Vector3 mouseDir)
+    {
+        Vector3 mouseDirNormal = mouseDir.normalized;
+        float xDir = Mathf.Atan2(mouseDirNormal.y, mouseDirNormal.x) * Mathf.Rad2Deg;
+
+        //if (_brain.MousePos.x - _brain.AgentTrm.position.x > 0)
+        //{
+        //    xDir += 180;
+        //}
+
+        // 플립 신경써줘야 함                      || _brain.MousePos랑 IsMine의 Player의 x랑 비교해서 플립시키기
+        // 대쉬 방향 신경써줘야 함                 || 
+        Vector3 dashPos = _brain.AgentTrm.position;
+        Vector3 dashRot = new Vector3(xDir, -90, -90);
+
+        ParticleManager.Instance.PlayParticleAll("Effect_PlayerDash", dashPos, dashRot);
     }
 
     public override void BrainFixedUpdate()
